25398e95e64a2355149b28492c1a5d0d3b3be79a
le1ezera <lkm19980706@gmail.com>
2024-05-20 10:19:19 +0800

    add pycharm leetcode plugin to take nots

---
+++ 每日一题/May/leetcode/editor/cn/[1542]找出最长的超赞子字符串.py
@@ -0,0 +1,67 @@
+# 给你一个字符串 s 。请返回 s 中最长的 超赞子字符串 的长度。 
+# 
+#  「超赞子字符串」需满足满足下述两个条件： 
+# 
+#  
+#  该字符串是 s 的一个非空子字符串 
+#  进行任意次数的字符交换后，该字符串可以变成一个回文字符串 
+#  
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入：s = "3242415"
+# 输出：5
+# 解释："24241" 是最长的超赞子字符串，交换其中的字符后，可以得到回文 "24142"
+#  
+# 
+#  示例 2： 
+# 
+#  输入：s = "12345678"
+# 输出：1
+#  
+# 
+#  示例 3： 
+# 
+#  输入：s = "213123"
+# 输出：6
+# 解释："213123" 是最长的超赞子字符串，交换其中的字符后，可以得到回文 "231132"
+#  
+# 
+#  示例 4： 
+# 
+#  输入：s = "00"
+# 输出：2
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  1 <= s.length <= 10^5 
+#  s 仅由数字组成 
+#  
+# 
+#  Related Topics 位运算 哈希表 字符串 👍 98 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def longestAwesome(self, s: str) -> int:
+        st = 0
+        d = {0: -1}
+        ans = 1
+        for i, c in enumerate(s):
+            v = int(c)
+            st ^= 1 << v
+            if st in d:
+                ans = max(ans, i - d[st])
+            else:
+                d[st] = i
+            for v in range(10):
+                if st ^ (1 << v) in d:
+                    ans = max(ans, i - d[st ^ (1 << v)])
+        return ans
+# leetcode submit region end(Prohibit modification and deletion)
