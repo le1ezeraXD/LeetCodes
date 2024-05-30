c335dd699717c61ec78e4e86409dc3e53b828a6b
le1ezera <lkm19980706@gmail.com>
2024-05-30 10:41:15 +0800

    每日一题: 找出出现至少三次的最长特殊子字符串II

---
+++ 每日一题/May/0530找出出现至少三次的最长特殊子字符串II.py
@@ -0,0 +1,27 @@
+'''给你一个仅由小写英文字母组成的字符串 s 。
+
+如果一个字符串仅由单一字符组成，那么它被称为 特殊 字符串。例如，字符串 "abc" 不是特殊字符串，而字符串 "ddd"、"zz" 和 "f" 是特殊字符串。
+
+返回在 s 中出现 至少三次 的 最长特殊子字符串 的长度，如果不存在出现至少三次的特殊子字符串，则返回 -1 。
+
+子字符串 是字符串中的一个连续 非空 字符序列。
+
+'''
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        groups = defaultdict(list)
+        cnt = 0
+        for i, ch in enumerate(s):
+            cnt += 1
+            if i + 1 == len(s) or ch != s[i + 1]:
+                groups[ch].append(cnt)  # 统计连续字符长度
+                cnt = 0
+
+        ans = 0
+        for a in groups.values():
+            a.sort(reverse=True)
+            a.extend([0, 0])  # 假设还有两个空串
+            ans = max(ans, a[0] - 2, min(a[0] - 1, a[1]), a[2])
+
+        return ans if ans else -1
