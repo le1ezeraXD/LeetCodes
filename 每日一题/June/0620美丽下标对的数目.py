23508e68108843b15e7c661356174f031cc73b66
le1ezera <lkm19980706@gmail.com>
2024-06-20 08:56:24 +0800

    每日一题: 美丽下标对的数目

---
+++ 每日一题/June/0620美丽下标对的数目.py
@@ -0,0 +1,18 @@
+'''
+给你一个下标从 0 开始的整数数组 nums 。如果下标对 i、j 满足 0 ≤ i < j < nums.length ，如果 nums[i] 的 第一个数字 和 nums[j] 的 最后一个数字 互质 ，则认为 nums[i] 和 nums[j] 是一组 美丽下标对 。
+
+返回 nums 中 美丽下标对 的总数目。
+
+对于两个整数 x 和 y ，如果不存在大于 1 的整数可以整除它们，则认为 x 和 y 互质 。换而言之，如果 gcd(x, y) == 1 ，则认为 x 和 y 互质，其中 gcd(x, y) 是 x 和 y 的 最大公因数
+'''
+
+
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if gcd(int(str(nums[i])[0]), int(str(nums[j])[-1])) == 1:
+                    res += 1
+
+        return res
