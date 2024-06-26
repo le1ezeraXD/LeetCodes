e74ea5579e5aeac1a3c2e79f26079bb44406f195
le1ezera <lkm19980706@gmail.com>
2024-06-26 11:45:47 +0800

    数据结构/深度优先搜索/二叉树最近公共祖先

---
+++ 数据结构/深度优先搜索/二叉树最近公共祖先.py
@@ -0,0 +1,30 @@
+'''给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+
+
+作者：LeetCode
+链接：https://leetcode.cn/leetbook/read/dfs/e81805/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left: return right
+        if not right: return left
+        return root
