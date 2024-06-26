b4244f94cb86767b0b5b25f59b7e7d5fc3b5f648
le1ezera <lkm19980706@gmail.com>
2024-06-26 11:24:45 +0800

    数据结构/深度优先搜索/求根到叶子结点数字之和

---
+++ 数据结构/深度优先搜索/求根到叶子节点数字之和.py
@@ -0,0 +1,32 @@
+'''给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+每条从根节点到叶节点的路径都代表一个数字：
+
+例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+计算从根节点到叶节点生成的 所有数字之和 。
+
+叶节点 是指没有子节点的节点。
+
+作者：LeetCode
+链接：https://leetcode.cn/leetbook/read/dfs/e84677/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, preTotal):
+            if not root:
+                return 0
+            preTotal = preTotal * 10 + root.val
+            if not root.left and not root.right:
+                return preTotal
+            return dfs(root.left, preTotal) + dfs(root.right, preTotal)
+
+        return dfs(root, 0)
